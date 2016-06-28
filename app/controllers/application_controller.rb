c33fada783b4c4a6fe2be5732d1ae37fad2000a9
Armen Rostamian <arostamian@ulive.com>
2016-06-28 14:35:58 -0400

    CRUDIFY, prevent mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+
+  before_action :ensure_user_not_mobile, except: :mobile
+
+  def mobile
+    render text: 'You cannot view the requested page from a mobile device'
+  end
+
+  private
+
+  def ensure_user_not_mobile
+    if request.user_agent =~ /mobile/
+      redirect_to controller: 'application', action: 'mobile'
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   get '/pets' => 'pets#index'
+  get 'pets/:id' => 'pets#show', as: :pet
+  post '/pets' => 'pets#create'
+  patch '/pets/:id' => 'pets#update'
+  delete 'pets/:id' => 'pets#destroy'
+
+  get '/mobile' => 'application#mobile'
 end
 
 
