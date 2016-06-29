8e3aed2f570c788484c9b2a74346fe401304b3df
Armen Rostamian <arostamian@ulive.com>
2016-06-29 09:09:40 -0400

    generate user model & db changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160628195624) do
+ActiveRecord::Schema.define(version: 20160629130854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20160628195624) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.datetime "last_seen_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
